6a332a93169f31dfcf26000a6d67f3f7c01763af
marcovitodev <m.pereira@acad.ifma.edu.br>
2025-02-18 05:25:31 -0300

    fix: change bg videdo

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,20 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            name: "[name]-[hash].[ext]",
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
